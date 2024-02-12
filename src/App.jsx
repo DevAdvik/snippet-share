4198c6a768f6acc5fb607918bcdd7c1ab63bb474
Advik <github@advik.dev>
2024-02-12 11:22:49 +0530

    Organized stylesheets and testing code syntax highlighting

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-import './App.css';
+import './styles/App.css';
 import rules from './rules';
 
 const applyMarkdownRules = (input) => {
--- src/CodeHighlight.jsx
+++ src/CodeHighlight.jsx
@@ -1,32 +1,38 @@
-import React from 'react';
+import { useState, StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
 import hljs from 'highlight.js/lib/common';
-import 'highlight.js/styles/base16/humanoid-dark.css';
+import 'highlight.js/styles/base16/hardcore.css';
 
 
-class CodeBlock extends React.Component {
-    componentDidMount() {
-        this.highlightCode();
+function CodeInput() {
+    const [inputCode, setInputCode] = useState("");
+    const handleCodeInput = ev => {
+        setInputCode(ev.target.value);
+        const elements = document.querySelectorAll("pre code");
+        elements.forEach(hljs.highlightBlock);
     }
 
-    componentDidUpdate() {
-        this.highlightCode();
-    }
 
-    highlightCode() {
-        const nodes = document.querySelectorAll('pre code');
-        nodes.forEach(node => hljs.highlightBlock(node));
-    }
-
-    render() {
-        const codestring = "const hey = `hey man`"
-        return (
-            <pre>
-                <code>
-                    {codestring};
-                </code>
-            </pre>
-        );
-    }
+    return (
+        <>
+            <div className="editCode">
+                <textarea name="userCode" value={inputCode} onChange={handleCodeInput}></textarea>
+                <button type='button'>Cancel</button>
+                <button type="button">Save</button>
+            </div>
+            <div className="preview">
+                <pre>
+                    <code>
+                        {inputCode}
+                    </code>
+                </pre>
+            </div>
+        </>
+    )
 }
 
-export default CodeBlock;
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+        <CodeInput />
+    </StrictMode>
+)
