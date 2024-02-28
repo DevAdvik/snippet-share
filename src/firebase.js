252f1275b0d30b9ed9589ebf3891fe1b95d146ad
Advik <github@advik.dev>
2024-02-28 15:47:54 +0530

    Changed .gitignore

--- src/firebase.js
+++ src/firebase.js
@@ -1,11 +1,3 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyCICXjK4sqoHl21q3Dpu4tSzJQq_Kb3hbQ",
-    authDomain: "snippet-sphere.firebaseapp.com",
-    projectId: "snippet-sphere",
-    storageBucket: "snippet-sphere.appspot.com",
-    messagingSenderId: "721094457461",
-    appId: "1:721094457461:web:873fcdbc5c1c7c83a5a03c",
-    measurementId: "G-X82RLXX6DC",
-};
+const firebaseConfig = {};
 
 export default firebaseConfig;
