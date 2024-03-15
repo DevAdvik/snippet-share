14ca2aefcc4d2d7719319e763f34fb6abe223f4c
harishveera29 <harishveeraraghavan29@gmail.com>
2024-03-15 10:34:11 +0530

    Added animations in homepage

--- src/LandingPage.jsx
+++ src/LandingPage.jsx
@@ -12,6 +12,10 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faReact, faSnapchat } from "@fortawesome/free-brands-svg-icons";
 import { useNavigate } from "react-router-dom";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import React, { useRef, useEffect } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+gsap.registerPlugin(ScrollTrigger);
 
 function LandingPage() {
     const navigator = useNavigate();
@@ -51,21 +55,10 @@ function LandingPage() {
                 </header>
 
                 <div className={styles.snippetDesc}>
-                    <div className={styles.snippetTexts}>
-                        <div className={styles.tagline}>
-                            <h1>Sharing Brilliance</h1>
-                            <h3>
-                                One <span>Snippet</span> at a Time.
-                            </h3>
-                        </div>
-                        <div className={styles.taglineText}>
-                            A seamless solution for sharing code <span>snippets</span>, enabling
-                            developers to connect, learn, and grow together.
-                        </div>
-                    </div>
-                    <div className={styles.snippets}>
-                        <img src={snippets} alt="err" />
-                    </div>
+                    <HomeContent />
+                    <HomeImage1 />
+
+
                 </div>
                 <div className={styles.stylingDiv}>
                     <img src={ellipse} alt="" />
@@ -76,22 +69,12 @@ function LandingPage() {
                 <h1>Revolutionizing Code Sharing</h1>
                 <div className={styles.textAndLaptop}>
                     <div className={styles.textContent}>
-                        <div className={styles.firstRow}>
-                            <img src={revolution} alt="Humans discussing different code snippets" />
-                            <p>
-                                In the ever-evolving landscape of software development,
-                                collaboration and knowledge sharing are paramount.
-                            </p>
-                        </div>
-                        <div className={styles.secondRow}>
-                            <p>
-                                Enter <span>Snippet Sphere</span>, a cutting-edge code snippet
-                                sharing web application poised to transform the way developers
-                                exchange and collaborate on code snippets.
-                            </p>
-                        </div>
+                        {/* dsfu */}
+                        <AboutContent1/>
+                        <AboutContent2/>
+                      
                     </div>
-                    <div className={styles.laptop}></div>
+                    <Lap />
                 </div>
             </section>
 
@@ -101,49 +84,10 @@ function LandingPage() {
                 </div>
 
                 <div className={featureStyles.content}>
-                    <div className={featureStyles.cards}>
-                        <div className={featureStyles.card12}>
-                            <div className={featureStyles.card1}>
-                                <h1>Empowering Collaboration</h1>
-                                <p>
-                                    Snippet Sphere enables seamless sharing of code snippets with
-                                    colleagues, peers, and the broader developer community through
-                                    easily generated public links.
-                                </p>
-                            </div>
-                            <div className={featureStyles.card2}>
-                                <h1>Syntax Highlighting for Maximum Clarity</h1>
-                                <p>
-                                    Snippet Sphere ensures code clarity and readability with robust
-                                    syntax highlighting for 10 languages directly within your
-                                    browser, fostering seamless development experiences.
-                                </p>
-                            </div>
-                        </div>
-                        <div className={featureStyles.card34}>
-                            <div className={featureStyles.card3}>
-                                <h1>Sleek and Intuitive Interface</h1>
-                                <p>
-                                    Experience the epitome of simplicity and elegance with Snippet
-                                    Sphere&apos;s sleek and intuitive interface. Effortlessly
-                                    navigate through your snippets with grace, allowing your
-                                    creativity to flow seamlessly.
-                                </p>
-                            </div>
-                            <div className={featureStyles.card4}>
-                                <h1>Future Enhancements</h1>
-                                <p>
-                                    The journey of Snippet Sphere is just beginning. In the pipeline
-                                    are plans for additional features and enhancements, including
-                                    collaborative editing, public snippet library, and integration
-                                    with popular development platforms.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={featureStyles.imageContainer}>
-                        <img src={phone} />
-                    </div>
+                   {/* sdvfyvsad */}
+                   <FeatureContent/>
+                   <FeatureImage/>
+                  
                 </div>
             </section>
 
@@ -228,4 +172,239 @@ function LandingPage() {
     );
 }
 
+
+function HomeContent() {
+
+    const h1 = useRef(null);
+    const content = useRef(null);
+
+    useEffect(() => {
+        gsap.to(h1.current, {
+            x: 0,
+            duration: 1,
+            delay: 0.3,
+        });
+    }, []);
+
+
+    useEffect(() => {
+        gsap.to(content.current, {
+            x: 0,
+            duration: 1,
+            delay: 0.5,
+        });
+    }, []);
+
+
+    return (
+        <div className={styles.snippetTexts}>
+            <div ref={h1} className={styles.tagline}>
+                <h1 >Sharing Brilliance</h1>
+                <h3 >
+                    One <span>Snippet</span> at a Time.
+                </h3>
+            </div>
+            <div ref={content} className={styles.taglineText}>
+                A seamless solution for sharing code <span>snippets</span>, enabling
+                developers to connect, learn, and grow together.
+            </div>
+        </div>
+    )
+}
+
+function HomeImage1() {
+    const img = useRef(null)
+    useEffect(() => {
+        gsap.to(img.current, {
+            scale: 1,
+            duration: 1.2,
+            delay: 0.7,
+        });
+    }, []);
+
+    return (
+        <div className={styles.snippets}>
+            <img ref={img} src={snippets} alt="err" />
+        </div>
+    )
+}
+
+function Lap() {
+    const img = useRef(null)
+    useEffect(() => {
+        gsap.to(img.current, {
+            x: 0,
+            duration: 1,
+            delay: 0.3,
+            scrollTrigger: {
+                trigger: img.current,
+            }
+        });
+    }, []);
+    return (
+        <div ref={img} className={styles.laptop}></div>
+    )
+}
+
+function AboutContent1() {
+
+    const content= useRef(null)
+    useEffect(() => {
+        gsap.to(content.current, {
+            x: 0,
+            duration: 1,
+            delay: 0.7,
+            scrollTrigger: {
+                trigger: content.current,
+            }
+        });
+    }, []);
+    return (
+        <div ref={content} className={styles.firstRow}>
+            <img src={revolution} alt="Humans discussing different code snippets" />
+            <p>
+                In the ever-evolving landscape of software development,
+                collaboration and knowledge sharing are paramount.
+            </p>
+        </div>
+
+    )
+}
+
+function AboutContent2() {
+
+    const content= useRef(null)
+    useEffect(() => {
+        gsap.to(content.current, {
+            x: 0,
+            duration: 0.7,
+            delay: 1,
+            scrollTrigger: {
+                trigger: content.current,
+            }
+        });
+    }, []);
+    return (
+        <div ref={content} className={styles.secondRow}>
+        <p>
+            Enter <span>Snippet Sphere</span>, a cutting-edge code snippet
+            sharing web application poised to transform the way developers
+            exchange and collaborate on code snippets.
+        </p>
+    </div>
+
+    )
+}
+function FeatureContent(){
+
+    const content1= useRef(null)
+    useEffect(() => {
+        gsap.to(content1.current, {
+            x: 0,
+            duration: 0.3,
+            delay: 0.8,
+            scrollTrigger: {
+                trigger: content1.current,
+            }
+        });
+    }, []);
+
+    const content2= useRef(null)
+    useEffect(() => {
+        gsap.to(content2.current, {
+            x: 0,
+            duration: 0.5,
+            delay: 1.1,
+            scrollTrigger: {
+                trigger: content2.current,
+            }
+        });
+    }, []);
+
+    const content3= useRef(null)
+    useEffect(() => {
+        gsap.to(content3.current, {
+            x: 0,
+            duration: 0.7,
+            delay: 1.4,
+            scrollTrigger: {
+                trigger: content3.current,
+            }
+        });
+    }, []);
+
+    const content4= useRef(null)
+    useEffect(() => {
+        gsap.to(content4.current, {
+            x: 0,
+            duration: 0.9,
+            delay: 1.7,
+            scrollTrigger: {
+                trigger: content4.current,
+            }
+        });
+    }, []);
+    return (
+        <div className={featureStyles.cards}>
+        <div className={featureStyles.card12}>
+            <div ref={content1}className={featureStyles.card1}>
+                <h1>Empowering Collaboration</h1>
+                <p>
+                    Snippet Sphere enables seamless sharing of code snippets with
+                    colleagues, peers, and the broader developer community through
+                    easily generated public links.
+                </p>
+            </div>
+            <div ref={content3} className={featureStyles.card2}>
+                <h1>Syntax Highlighting for Maximum Clarity</h1>
+                <p>
+                    Snippet Sphere ensures code clarity and readability with robust
+                    syntax highlighting for 10 languages directly within your
+                    browser, fostering seamless development experiences.
+                </p>
+            </div>
+        </div>
+        <div className={featureStyles.card34}>
+            <div ref={content2}className={featureStyles.card3}>
+                <h1>Sleek and Intuitive Interface</h1>
+                <p>
+                    Experience the epitome of simplicity and elegance with Snippet
+                    Sphere&apos;s sleek and intuitive interface. Effortlessly
+                    navigate through your snippets with grace, allowing your
+                    creativity to flow seamlessly.
+                </p>
+            </div>
+            <div ref={content4} className={featureStyles.card4}>
+                <h1>Future Enhancements</h1>
+                <p>
+                    The journey of Snippet Sphere is just beginning. In the pipeline
+                    are plans for additional features and enhancements, including
+                    collaborative editing, public snippet library, and integration
+                    with popular development platforms.
+                </p>
+            </div>
+        </div>
+    </div>
+    )
+}
+function FeatureImage(){
+
+    const img= useRef(null)
+    useEffect(() => {
+        gsap.to(img.current, {
+            scale:1,
+            duration: 0.7,
+            delay: 2,
+            scrollTrigger: {
+                trigger: img.current,
+            }
+        });
+    }, []);
+
+    return(
+        <div ref={img}className={featureStyles.imageContainer}>
+        <img src={phone} />
+    </div>
+    )
+}
 export default LandingPage;
